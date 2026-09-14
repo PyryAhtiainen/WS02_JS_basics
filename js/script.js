10725d6c6fd38de87e51e18cc8c4308b58dd5399
Pyry Ahtiainen <33270345+PyryAhtiainen@users.noreply.github.com>
2026-09-14 19:23:19 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,17 @@ console.log("Hello World!");
 
 
 const name = "Pyry"; let age = 29; const favouriteAnimal = "Elephant";
-console.log("Hello! My name is " + name + "and my favourite animal is the " + favouriteAnimal + "."
+console.log("Hello! My name is " + name + " and my favourite animal is the " + favouriteAnimal + ".");
+
+const userName = prompt("Please write your name here:");console.log("Hello " + userName + ", pleased to have you here!");
+
+const userAge = prompt("Please type your age in number format with no spaces:");
+if (userAge >= 18) {    console.log("You are " + userAge + " years old so you are an adult.");}else console.log("You are " + userAge + " years old so you are under 18, and NOT an adult.");
+
+function greetUser(userName) { console.log("Hello " + userName + "! "); }
+greetUser(userName);greetUser("Betty");greetUser("Kaapo"),greetUser("Yousef");
+
+
+function clickFunction() {
+  alert("You have succesfully clicked the button " + userName + "!");
+}
