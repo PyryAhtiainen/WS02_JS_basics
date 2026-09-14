e850e9dcdc8210322dcd304ed6e7817101164304
Pyry Ahtiainen <33270345+PyryAhtiainen@users.noreply.github.com>
2026-09-14 18:14:41 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 console.log("Hello World!");
+
+
+const name = "Pyry"; let age = 29; const favouriteAnimal = "Elephant";
+console.log("Hello! My name is " + name + "and my favourite animal is the " + favouriteAnimal + "."
